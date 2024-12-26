651a423e79b43b1508df7d6a30a434c7f9cbf6f2
Marco Antonio Barro Tortelli <marcobarrotortelli@gmail.com>
2024-12-26 01:13:29 -0300

    remove user

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 import { errorHandler } from './middlewares/error.handler';
 import authRoutes from './routes/auth.routes';
-import userRoutes from './routes/user.routes';
 const app = express();
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
-app.use('/api', userRoutes)
 app.use(errorHandler);
 
 export default app;
